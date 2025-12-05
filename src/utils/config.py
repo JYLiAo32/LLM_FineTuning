bc2700e3d382dcf71a295c8cb4f7e7a5095eb44d
ljy <liaojy28@mail2.sysu.edu.cn>
2025-12-05 15:56:13 +0800

    fix lora_dir config error

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,39 +1,38 @@
-from unsloth import is_bfloat16_supported
 import datetime
+
+def _gen_lora_dir():
+    current_time = datetime.datetime.now()
+    timestamp = current_time.strftime("%y%m%d_%H%M%S")
+    return f"./lora_model/{timestamp}/"
 
 class GlobalConfig:
     seed = 32
     max_seq_length = 2048
-    dtype = None             # None -> auto, or torch.float16 / torch.bfloat16 etc.
+    dtype = None  # None -> auto, or torch.float16 / torch.bfloat16 etc.
     load_in_4bit = False
-    
-    @property
-    def lora_dir(self):
-        current_time = datetime.datetime.now()
-        timestamp = current_time.strftime("%y-%m-%d %H-%M-%S")
-        return f"./lora_model/{timestamp}/"
-    
+    lora_dir = _gen_lora_dir()
+
 class SFTConfig(GlobalConfig):
-    base_model_path = '/data/ljy/models/unsloth/Qwen2.5-7B-Instruct'
-    per_device_train_batch_size = 2,
-    gradient_accumulation_steps = 4,
-    warmup_steps = 5,
-    max_steps = 60,
-    learning_rate = 2e-4,
-    fp16 = not is_bfloat16_supported(),
-    bf16 = is_bfloat16_supported(),
-    logging_steps = 1,
-    optim = "adamw_8bit",
-    weight_decay = 0.01,
-    lr_scheduler_type = "linear",
-    output_dir = "outputs",
-    report_to = "none", # Use this for WandB etc
+    base_model_path = "/data/ljy/models/unsloth/Qwen2.5-7B-Instruct"
+    per_device_train_batch_size = 2
+    gradient_accumulation_steps = 4
+    warmup_steps = 5
+    max_steps = 60
+    learning_rate = 2e-4
+    logging_steps = 1
+    optim = "adamw_8bit"
+    weight_decay = 0.01
+    lr_scheduler_type = "linear"
+    output_dir = "outputs"
+    report_to = "none"  # Use this for WandB etc
+
 
 class InferenceConfig(GlobalConfig):
     DEFAULT_MAX_NEW_TOKENS = 256
     DEFAULT_TEMPERATURE = 0.7
     DEFAULT_TOP_P = 0.95
-    
+
+
 class PromptConfig:
     alpaca_prompt = """Below is an instruction that describes a task, 
     paired with an input that provides further context. 
@@ -45,3 +44,6 @@ class PromptConfig:
     ### Response:
     {output}
     """
+
+if __name__ == "__main__":
+    print(SFTConfig.lora_dir)
